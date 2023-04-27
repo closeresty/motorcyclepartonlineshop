4bd729f8ff4fefae90a4d17022729edbcd55bad9
devres <potolinresty110394@gmail.com>
2023-04-27 19:20:15 +0800

    update for home

---
+++ components/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=shop_db';
+$user_name = 'root';
+$user_password = '';
+// qw3rtyQW3RTY@123.c0m
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> Reservex</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact us</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="tel:09153858690"><i class="fas fa-phone"></i> +639153858690</a>
+         <a href="tel:09165306030"><i class="fas fa-phone"></i> +639165306030</a>
+         <a href="mailto:potolin110394gmail.com"><i class="fas fa-envelope"></i> potolinresty110394@gmail.com</a>
+         <a href="https://whatismyipaddress.com/"><i class="fas fa-map-marker-alt"></i>Rodriguez Rizal, Philippines</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy;©Copyright 2022<?= date('Y'); ?>All rights reserved.<span>Powered by</span> | Devres</div>
+
+</footer>
